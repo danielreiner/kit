70b122e6f731a8b777a10b66404ac081e605b6ce
David Reissner <d.reissner@gmail.com>
2013-03-18 18:56:03 +0100

    add custom page with id home to root resource

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Kit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "pages#show", :id => 'home'
 
   # See how all your routes lay out with "rake routes"
 
