2913445ccd007bb982e619400f02e15cb128b643
David Reissner <d.reissner@gmail.com>
2013-04-02 05:35:48 +0200

    First styling approaches for artist page

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,20 @@
 class Artist < ActiveRecord::Base
+
+  scope :sorted, :order => "last_name ASC"
+
   attr_accessible :alias, :birthday, :category, :description, :first_name, :last_name, :nationality, :arts_attributes
 
   has_many :arts, :as => :attachable
 
   accepts_nested_attributes_for :arts
+
+  
+  def fullname
+    "#{first_name} #{last_name}".strip
+  end
+
+  def reverse_fullname
+    "#{last_name}, #{first_name}".strip
+  end
+
 end
