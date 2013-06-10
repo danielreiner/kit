fe63f6ee7b4172701f46bfd7b3c7090ab151b9d0
David Reissner <d.reissner@gmail.com>
2013-06-10 21:20:18 +0200

    Try to fix default.js error

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'devise'
 require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Kit
@@ -61,7 +62,6 @@ module Kit
     config.assets.precompile << '*.js'
 
     config.assets.initialize_on_precompile = false
-
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
